8947c3b4827a778fac70119fffaa5c87fd46d1fd
BALMUSDAQ-STUDIO <balmuzdaq.studio@gmail.com>
2024-11-08 21:50:58 +0500

    first

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+import icons from "./icons.js"
+import data from "./data.js"
+import images from "./images.js"
+
+export {
+    icons,
+    data,
+    images
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        /* blue:"ff3ff4*/
+        /* blue:"ff3ff4*/
+      },
+      fontFamily:{
+        /*intro: ["intro", "sans-serif"],
+        monoton: ["monoton", "sans-serif"],
+        patua: ["patua", "sans-serif"],
+        rubik: ["rubik", "sans-serif"],
+        rubikOne: ["rubikOne", "sans-serif"]*/
+      }
+    },
+  },
+  plugins: [],
+}
